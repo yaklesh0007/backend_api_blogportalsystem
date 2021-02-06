635d12af44d02138c862d6b5fcc6459698d5e688
aklesh <yaklesh0007@gmail.com>
2021-02-06 14:05:48 +0545

    changes to Vote route to Like route

--- routes/likeRoute.js
+++ routes/likeRoute.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const router=express.Router();
-const Vote=require('../models/Vote')
+const Like=require('../models/Like')
 const {check,validationResult}=require('express-validator')
 
 module.exports=router
