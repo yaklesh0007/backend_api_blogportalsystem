5289b6b6b903f8af7943fdacbb7683f4244eff5e
aklesh <yaklesh0007@gmail.com>
2021-02-11 22:19:17 +0545

    deleted like and dislke models,routes because it seems to be unneccessary

--- routes/dislikeRoute.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const Dislike=require('../models/Dislike')
-const {check,validationResult}=require('express-validator')
-const authentication=require('../middleware/authentication')
-
-module.exports=router
--- routes/likeRoute.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const Like=require('../models/Like')
-const {check,validationResult}=require('express-validator')
-const authentication=require('../middleware/authentication')
-
-module.exports=router
